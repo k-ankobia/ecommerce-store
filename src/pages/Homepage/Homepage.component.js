f64f6228166fc655e231b6b6ea07a5fd92596b36
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-14 01:11:12 +0100

    Feat(Components): refactored page into different components

---
+++ src/pages/Homepage/Homepage.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directories from "../../components/DirectoriesCard/DirectoriesCard";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <Directories />
+    </div>
+  );
+};
