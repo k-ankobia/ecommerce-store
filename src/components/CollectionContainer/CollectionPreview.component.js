7a0a1ab45e95b58aa783ff6a923ffcf7721d18f2
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-15 09:13:30 +0100

    feat(72-collectionItem): add collection item container & styling

--- src/components/CollectionContainer/CollectionPreview.component.js
+++ src/components/CollectionContainer/CollectionPreview.component.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./collectionPreview.styles.scss";
+import { CollectionItem } from "../CollectionItem/CollectionItem";
 
 export const CollectionPreview = ({ title, items }) => {
   return (
-    <div className="collection">
+    <div className="collection-container">
       <h1 className="title"> {title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div key={item.id}> {item.name}</div>
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))}
       </div>
     </div>
