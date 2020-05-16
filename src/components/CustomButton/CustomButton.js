5fc24ffaca00fb1eef7b163fccc4dd1a9c6d6abb
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-16 10:20:10 +0100

    cleanup console.logs

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./CustomButtom.styled.scss";
 // clidren is being used for all text within the component
 export const CustomButton = ({ children, ...otherProps }) => {
-  console.log(otherProps);
-
-  //   console.log("spread props", ...otherProps);
-
   return (
     <button className="custom-button" {...otherProps}>
       {children}
