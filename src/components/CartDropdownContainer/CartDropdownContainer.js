11506d17efe456f9620d2ed166c3626e43923902
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-23 07:51:28 +0100

    feat(cart hidden): added dispatch prop to toggle cart hiden on click of button

--- src/components/CartDropdownContainer/CartDropdownContainer.js
+++ src/components/CartDropdownContainer/CartDropdownContainer.js
@@ -5,11 +5,11 @@ import { createStructuredSelector } from "reselect";
 import { CustomButton } from "../CustomButton/CustomButton";
 import { CartDropdownItem } from "../cartDropdownItem/cartDropdownItem";
 import { withRouter } from "react-router-dom";
-
+import { toggleCartHidden } from "../../redux/cart/cart.action";
 import { selectCartItems } from "../../redux/cart/cart.reselecotors";
 import "./cartDropdownContainer.styles.scss";
 
-const CartDropdownContainer = ({ cartItems, history }) => {
+const CartDropdownContainer = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -21,7 +21,12 @@ const CartDropdownContainer = ({ cartItems, history }) => {
           <span className="empty-message">YOUR CART IS EMPTY</span>
         )}
       </div>
-      <CustomButton onClick={() => history.push("./checkout")}>
+      <CustomButton
+        onClick={() => {
+          history.push("./checkout");
+          dispatch(toggleCartHidden());
+        }}
+      >
         {" "}
         GO TO CHECKOUT
       </CustomButton>
