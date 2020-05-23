2e0b92035d4d61d29a746ad64496a941e9436739
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-23 06:53:51 +0100

    feat(checkout header) add checkout header  and stylilng

--- src/pages/checkout/checkout.componet.js
+++ src/pages/checkout/checkout.componet.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./chekcout.styles.scss";
 const CheckoutPage = () => {
-  return <div>CHECKOUT PAGE</div>;
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">PRODUCT</div>
+        <div className="header-block">DESCRIPTION</div>
+        <div className="header-block">QUANTITY</div>
+        <div className="header-block">PRICE</div>
+        <div className="header-block">REMOVE</div>
+      </div>
+    </div>
+  );
 };
 export default CheckoutPage;
