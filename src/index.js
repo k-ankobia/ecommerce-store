223a21e19be5a4646b5f46f610c0dc9bc6337de7
kwaku ankobia <9341964+k-ankobia@users.noreply.github.com>
2020-05-23 20:53:38 +0100

    Feat 129: Redux-persist add redux persist to root reducer for local storage

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Browser } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <Browser>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Browser>
     </React.StrictMode>
   </Provider>,
